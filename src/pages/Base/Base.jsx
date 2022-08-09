e18378c9a1e3fd25ec5e582691f50671b6f76796
Luisana Perez <luisanahenmaryp@gmail.com>
2022-08-09 19:04:56 -0400

    New things

--- src/pages/Base/Base.jsx
+++ src/pages/Base/Base.jsx
@@ -1,3 +1,4 @@
+import { useEffect} from 'react'
 import { Outlet } from "react-router-dom"
 import GaugeTemper from "../../components/GaugeTemper/GaugeTemper"
 import { useSelector, useDispatch } from "react-redux";
@@ -6,13 +7,17 @@ import { setData } from "../../reducers/slices/Gauge";
 const Base = () => {
     const { data } = useSelector((state) => state.reducerAll.Gauge);
     const dispatch = useDispatch()
+
+    useEffect(()=>{
+        const valueGauge = 50 //aqui puede ir el valor que saque de firebase
+        dispatch(setData({temperature:valueGauge}))
+    },[])
     
 
     return (
         <div>
             <Outlet />
             <GaugeTemper gauge_data={data} />
-            <button onClick={()=>dispatch(setData({temperature:20}))} >Set 20</button>
         </div>
     )
 }
